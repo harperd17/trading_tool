db4ff49d5565cdf8a66e029de0880996cfb7c431
harperd17 <70535252+harperd17@users.noreply.github.com>
2022-02-03 21:03:57 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='traing_tool',
+      version='0.1',
+      description='Library for Collecting data and building trading strategies',
+      author='David Harper',
+      author_email='spice.algo.man@gmail.com',
+      packages=['stock_data_collector','backtester','trading_strategies'],
+     )
