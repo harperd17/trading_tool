4e01f7567ce6d5724452281980fa8c983bc00e32
harperd17 <70535252+harperd17@users.noreply.github.com>
2022-02-03 21:04:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='traing_tool',
+setup(name='trading_tool',
       version='0.1',
       description='Library for Collecting data and building trading strategies',
       author='David Harper',
