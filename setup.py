93a3c226a38023333b1d85a4e4f522b5f8ec995c
harperd17 <70535252+harperd17@users.noreply.github.com>
2022-02-04 11:31:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(name='trading_tool',
       version='0.1',
-      description='Library for Collecting data and building trading strategies',
+      description='Library for Collecting data, building trading strategies, and backtesting them.',
       url = 'https://github.com/harperd17/trading_tool',
       author='David Harper',
       author_email='spice.algo.man@gmail.com',
-      #package_dir = {'': 'trading_tool'},
-      packages=['trading_tool','trading_tool.stock_data_collector','trading_tool.backtester','trading_tool.trading_strategies'],
+      package_dir = {"": "trading_tool"},
+      packages = find_packages(where="trading_tool")
+      #packages=['trading_tool','trading_tool.stock_data_collector','trading_tool.backtester','trading_tool.trading_strategies'],
      )
