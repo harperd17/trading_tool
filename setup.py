15d6f507060efc83286a04c5c4e93983e2d095c7
harperd17 <70535252+harperd17@users.noreply.github.com>
2022-02-03 21:20:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='trading_tool',
       url = 'https://github.com/harperd17/trading_tool',
       author='David Harper',
       author_email='spice.algo.man@gmail.com',
-      py_modules=['stock_data_collector','backtester','trading_strategies'],#packages
+      #py_modules=['stock_data_collector','backtester','trading_strategies'],#packages
      )
