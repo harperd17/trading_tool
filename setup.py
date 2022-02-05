73c2ceca19a8646e6a798f33d7931ece0b84e2cc
harperd17 <70535252+harperd17@users.noreply.github.com>
2022-02-04 19:29:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 with open('requirements.txt') as f:
-    required_packages = f.read()#.splitlines()
+    required_packages = f.read().splitlines()
 
 setup(name='trading_tool',
       version='0.1',
@@ -11,6 +11,6 @@ setup(name='trading_tool',
       author_email='spice.algo.man@gmail.com',
       package_dir = {"": "trading_tool"},
       packages = find_packages(where="trading_tool"),
-      install_reqs = required_packages,
+      install_requires = required_packages,
       #packages=['trading_tool','trading_tool.stock_data_collector','trading_tool.backtester','trading_tool.trading_strategies'],
      )
