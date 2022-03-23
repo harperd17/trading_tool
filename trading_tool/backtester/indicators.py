473cbfa6443e1b26846dbadfe809f765aff2087f
harperd17 <70535252+harperd17@users.noreply.github.com>
2022-03-22 21:50:37 -0400

    Create indicators.py

---
+++ trading_tool/backtester/indicators.py
@@ -0,0 +1,28 @@
+import backtrader as bt
+
+class SMA(bt.Indicator):
+    lines = ('sma',)
+
+    params = (('period', 15),)
+
+    def __init__(self):
+        self.lines.sma = bt.talib.SMA(self.data, period=self.p.period)
+
+    def matrix_method(data, period):
+      # this method isn't actually meant to be used by this class, but just to be able to link the method
+      # for calculating the indicator
+        return talib.SMA(data['Close'],timeperiod=period)
+      
+      
+class EMA(bt.Indicator):
+    lines = ('ema',)
+
+    params = (('period', 15),)
+
+    def __init__(self):
+        self.lines.ema = bt.talib.EMA(self.data, period=self.p.period)
+
+    def matrix_method(data, period):
+      # this method isn't actually meant to be used by this class, but just to be able to link the method
+      # for calculating the indicator
+        return talib.EMA(data['Close'],timeperiod=period)
